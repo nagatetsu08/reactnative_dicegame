7783df208d62be57d19ce86526a0e1e857eed528
永田哲也 <nagatatetsuya@nagatatetsuyanoMacBook-Air.local>
2025-08-31 20:40:37 +0900

    ログリスト実装

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -13,25 +15,59 @@ export default function App() {
 
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
+
+  // カスタムフォント
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  // カスタムフォントがロードされるまでAppLoadingコンポーネントを表示する
+  if(!fontsLoaded) {
+
+  }
+
+
+  /**
+   * handlerとしてまとめておくことで関数を子コンポーネントに渡した時にやっておきたい処理を 
+   * グループ化できる。渡す際はhandler名を渡すだけで、引数などは呼び出し先のコンポーネント内で
+   * 定義してやればいい。gameOverHandlerが一番参考になる。
+   * 
+   */
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
     setGameIsOver(false);
   }
 
-  function gameOverHandler() {
+  function gameOverHandler(numberOfRounds) {
     setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  function startNewGameHandler() {
+    setGuessRounds(0);
+    setUserNumber(null);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
+    // 遷移先でユーザーとCPUの数が同じだった時のみ、gameOverHandlerを実行している。
+    // ここでは関数を渡すだけ
     screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
   }
 
   if(gameIsOver && userNumber) {
-    screen = <GameOverScreen onGameOver={gameOverHandler} />;
+    screen = (
+      <GameOverScreen 
+        userNumber={userNumber} 
+        roundsNumber={guessRounds} 
+        onStartNewGame={startNewGameHandler} 
+        />
+    );
   }
 
 
---
+++ components/game/GuessLogItem.js
@@ -0,0 +1,37 @@
+
+import { View, Image, StyleSheet, Text } from "react-native";
+
+import Colors from "../../constants/colors";
+
+function GuessLogItem({ roundNumber, guess }) {
+    return (
+        <View style={styles.listItem}>
+            <Text style={styles.itemText}>Round: {roundNumber}</Text>
+            <Text style={styles.itemText}>Guess: {guess}</Text>
+        </View>
+    );
+}
+
+export default GuessLogItem;
+
+const styles = StyleSheet.create({
+    listItem: {
+        borderColor: Colors.primary800,
+        borderWidth: 1,
+        borderRadius: 40,
+        padding: 12,
+        marginVertical: 8,
+        backgroundColor: Colors.accent500,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        elevation: 4,
+        shadowColor: Colors.black,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 3,
+        shadowOpacity: 0.25,
+    },
+    itemText: {
+        fontFamily: 'open-sans',
+    }
+});
